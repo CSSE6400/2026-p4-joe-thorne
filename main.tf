f61a16a2cd571f0ef9d142e028e8b6d493510916
Joseph Thorne <joseph.thorne@student.uq.edu.au>
2026-03-17 13:25:35 +1000

    completed extension, ami reverted to original to pass unit test

--- main.tf
+++ main.tf
@@ -57,6 +57,31 @@ resource "aws_security_group" "hextris-server" {
   }
 }
 
+data "aws_ami" "latest" {
+  most_recent = true
+  owners = ["amazon"]
+
+  filter {
+    name = "name"
+    values =  ["al2023-ami-2023*"]
+  }
+
+    filter {
+    name = "root-device-type"
+    values = ["ebs"]
+  }
+
+ filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+
+ filter {
+    name = "architecture"
+    values = ["x86_64"]
+  }
+}
+
 output "hextris-url" {
   value = "http://${aws_instance.hextris-server.public_ip}/"
 }
